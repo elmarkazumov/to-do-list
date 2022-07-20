3d263da57359261002280e4b56b0a72ddeca607a
elmarkazumov <elmar.csgl@mail.ru>
2022-07-20 19:37:20 +0500

    working functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 let input_entry = document.querySelector('#target_entry'), btn_sumb = document.querySelector('#target_sumb');
-let array = [];
 
 function entry(text){
-    array.push(text);
-    console.log(Object.entries(array));
-    document.querySelector('.main__tasks').innerHTML += "<p>" + text + "</p>";
+    let task = document.createElement('div');
+    task.classList = 'main__task';
+    
+    let task_checkbox = document.createElement('input');
+    task_checkbox.setAttribute("type", "checkbox");
+
+    let task_text = document.createElement('p');
+    task_text.append(text);
+
+    task.append(task_checkbox);
+    task.append(task_text);
+
+    document.querySelector('.main__tasks').append(task);
+
+    task_checkbox.addEventListener("click", function() {
+      if(task_checkbox.checked == true){
+        task.classList.add("main__inactive");
+        task.setAttribute("contenteditable", "false");
+      } else{
+        task.classList.remove("main__inactive"); 
+      }
+
+    });
 }
 
 btn_sumb.onclick = function(){
